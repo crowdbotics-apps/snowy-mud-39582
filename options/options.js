5283ca53d5ca15eee6ff7a56fa871bf8b3f90db3
mohd suhel <withskfun@gmail.com>
2023-03-28 19:31:13 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "snowy-mud-39582",
-  url: "https://snowy-mud-39582.botics.co",
-  api: "https://snowy-mud-39582.botics.co/api/v1"
-}
+  "name": "snowy-mud-39582",
+  "url": "https://snowy-mud-39582.botics.co",
+  "api": "https://snowy-mud-39582.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,30 @@
+import { TextInput } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }} style={styles.OwdxXXnH}><TextInput style={styles.eivliGVW}></TextInput></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  OwdxXXnH: {
+    borderRadius: 0
+  },
+  eivliGVW: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 336,
+    height: 42
+  }
+});
+export default Untitled1;
